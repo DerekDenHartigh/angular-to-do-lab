3594b65b59b70537082b9441193211ad103de74f
Derek <derekdenhartigh@gmail.com>
2019-04-04 16:46:57 -0400

    did some major styling (somewhat obnoxious or fun, depending) still can't get AngularJS to populate the list however, I see an object flash there for a second before disappearing.

--- app.controller.js
+++ app.controller.js
@@ -13,6 +13,16 @@
 //     ]
 //   };
 
+TodoApp.controller('TodoController', function($scope){ // are arrow functions ok for angular?
+  $scope.todoList = [
+  {task:"Wake Up!", completed:true},
+  {task:"Grab your brush and put a little makeup!", completed:true},
+  {task:"Hide the scars to fade away the shakeup!", completed: false},
+  {task:"Grab your keys off the table!", completed: false},
+  {task:"Create another fable!", completed: true}
+]
+});
+
   /* notes from: https://docs.angularjs.org/guide/scope
 
   angular.module('scopeExample', [])

a409ad512e6c27d7d7af82abc6c9e07c25424b26
Derek <derekdenhartigh@gmail.com>
2019-04-14 15:48:43 -0400

    working on getting the add button working w/ function binding

--- components/todo-add/todo-add.js
+++ components/todo-add/todo-add.js
@@ -1,26 +1,19 @@
 "use strict";
 function TodoAdd(){
     const controller = this;
-    controller.addTask = function(newTask){
-      if(newTask==="" || newTask===undefined){  // added this so you can't add empty/undefined tasks
-      return;
-      }
-      else{
-      controller.TodoList.push({task: newTask, completed: false});
-      console.log(newTask);
-      };
-    };
+
 }
 
 angular.module('TodoApp').component('todoAdd', {
     template:`
     <form id="add-form" novalidate>
     <input id="add-to-to-do" class="input-field" type="text" placeholder="Could you also..." ng-model="$ctrl.newTask">
-    <button id="add-button" type="submit" ng-click="$ctrl.addTask(newTask)">Add 2 To Do</button>
+    <button id="add-button" type="submit" ng-click="$ctrl.addTask($ctrl.newTask)">Add 2 To Do</button>
     </form>
    `,
         controller: TodoAdd,
         bindings: {
-          newTask: "="
+          // newTask: "<", don't need, gets passed into bound function.
+          addTask: "&" // & for function binding
         }
 });
--- components/todo-list/todo-list.js
+++ components/todo-list/todo-list.js
@@ -1,7 +1,7 @@
 "use strict";
 function TodoList(){
     const controller = this;
-    let newTask, search, removeItem, completeTask, editItem, addTask;
+    // let newTask, search, removeItem, completeTask, editItem, addTask;
   controller.TodoList = [
     {task:"Wake Up!", completed: false},
     {task:"Grab your brush and put a little makeup!", completed: false},
@@ -10,7 +10,15 @@ function TodoList(){
     {task:"Create another fable!", completed: false}
   ];
 
-
+  controller.addTask = function(newTask){
+    if(newTask==="" || newTask===undefined){  // added this so you can't add empty/undefined tasks
+    return;
+    }
+    else{
+    controller.TodoList.push({task: newTask, completed: false});
+    console.log(newTask);
+    };
+  };
 
 }
 
@@ -21,7 +29,7 @@ angular.module('TodoApp').component('todoList', {
       <h1 id="h1">Honey-Do List:</h1>
       <h2 id="h1">would you kindly...</h2>
       <todo-filter search="$ctrl.search"></todo-filter>
-      <todo-tasks newTask="crtl.newTask" search="$ctrl.search" todo-list="$ctrl.TodoList"></todo-tasks>
+      <todo-tasks addTask="$crtl.addTask" search="$ctrl.search" todo-list="$ctrl.TodoList"></todo-tasks>
       <todo-add newTask="$ctrl.newTask"></todo-add>
     </div>
     </main>

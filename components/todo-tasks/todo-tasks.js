0b555a33cb2c802c90202240d2d663f787428807
Derek <derekdenhartigh@gmail.com>
2019-04-22 14:32:11 -0400

    figured it out, needed to use {item: item} in

--- components/todo-tasks/todo-tasks.js
+++ components/todo-tasks/todo-tasks.js
@@ -8,15 +8,15 @@ angular.module('TodoApp').component('todoTasks', {
     template:`
     <ul id="to-do-list">
     <li class="list-item" ng-repeat="item in $ctrl.todoList | filter:$ctrl.search">
-      <div class="complete-button" ng-click="$ctrl.completeTask(item)" ng-if="!item.completed">Finished!</div>
+      <div class="complete-button" ng-click="$ctrl.completeTask({item: item})" ng-if="!item.completed">Finished!</div>
       <span class="task" ng-model="listItem" ng-class="{'completed':item.completed===true}">{{item.task}}</span>
       <i class="material-icons edit" ng-click="item.toggle=true">edit</i>
-      <i class="material-icons x" ng-click="$ctrl.removeItem(item)">close</i>
+      <i class="material-icons x" ng-click="$ctrl.removeItem({item: item})">close</i>
       
       <div id="pop-up-edit" ng-if="item.toggle">
         What you meant was: <input ng-model="taskEdit" class="input-field" id="edit-field">
         <!--<button type="submit" id="change-it-button" ng-click="$ctrl.editItem(item, taskEdit)">Change it!</button>-->
-        <button type="submit" id="change-it-button" ng-click="$ctrl.editItem({item: $ctrl.item}, {taskEdit: $ctrl.taskEdit})">Change it!</button>
+        <button type="submit" id="change-it-button" ng-click="$ctrl.editItem({item: item, taskEdit: taskEdit})">Change it!</button>
         <i class="material-icons x" id="pop-up-exit" ng-click="item.toggle=!item.toggle">close</i>
       </div>
 
